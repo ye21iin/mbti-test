9e083099cf25b35db4a3676f5546ce70350f559a
ye21iin <kathykr120@gmail.com>
2025-02-20 20:19:22 +0900

    feat: setup API

---
+++ src/api/auth.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const API_URL = import.meta.env.BASE_URL;
+
+export const register = async (userData) => {
+  const response = await axios.post(`${API_URL}/register`, userData);
+  return response.data;
+};
+
+const EXP_TM = "10m";
+export const login = async (userData) => {
+  const response = await axios.post(
+    `${API_URL}/login?expiresIn=${EXP_TM}`,
+    userData
+  );
+  return response.data;
+};
+
+export const getUserProfile = async (token, id) => {
+  const response = await axios.get(`${API_URL}/user?user_id=${id}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
+
+export const updateProfile = async (token, formData) => {
+  const response = await axios.patch(`${API_URL}/profile`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  return response.data;
+};
