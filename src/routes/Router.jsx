55876451509342042d8b4b2c9dcffd2b844459c5
ye21iin <kathykr120@gmail.com>
2025-02-20 11:36:58 +0900

    Init: setup Routing backbone

---
+++ src/routes/Router.jsx
@@ -0,0 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Results from "../pages/Results";
+import Test from "../pages/Test";
+import Profile from "../pages/Profile";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
