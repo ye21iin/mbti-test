0d61d863ae433dcc94a392f13e6a27ca32a3afbd
ye21iin <kathykr120@gmail.com>
2025-02-22 00:21:19 +0900

    refactor: edit API call logic regarding auth

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,52 +2,34 @@ import axios from "axios";
 
 const API_URL = import.meta.env.VITE_AUTH_BASE_URL;
 
-export const register = async (userData) => {
-  try {
-    const response = await axios.post(`${API_URL}/register`, userData);
-    return response.data;
-  } catch (error) {
-    throw new Error(error);
-  }
-};
+export const register = (userData) =>
+  axios
+    .post(`${API_URL}/register`, userData)
+    .then((res) => res.data)
+    .catch(console.error);
 
 const EXP_TM = "20m";
-export const login = async (userData) => {
-  try {
-    const response = await axios.post(
-      `${API_URL}/login?expiresIn=${EXP_TM}`,
-      userData
-    );
-    return response.data;
-  } catch (error) {
-    throw new Error(error);
-  }
-};
+export const login = (userData) =>
+  axios
+    .post(`${API_URL}/login?expiresIn=${EXP_TM}`, userData)
+    .then((res) => res.data)
+    .catch(console.error);
 
-export const getUserProfile = async (token, id) => {
-  try {
-    const url = id ? `${API_URL}/user?user_id=${id}` : `${API_URL}/user`;
-    const response = await axios.get(url, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    return response.data;
-  } catch (error) {
-    throw new Error(error);
-  }
-};
+export const getUserProfile = (token, id) =>
+  axios
+    .get(id ? `${API_URL}/user?user_id=${id}` : `${API_URL}/user`, {
+      headers: { Authorization: `Bearer ${token}` },
+    })
+    .then((res) => res.data)
+    .catch(console.error);
 
-export const updateProfile = async (token, formData) => {
-  try {
-    const response = await axios.patch(`${API_URL}/profile`, formData, {
+export const updateProfile = (token, formData) =>
+  axios
+    .patch(`${API_URL}/profile`, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
         Authorization: `Bearer ${token}`,
       },
-    });
-    return response.data;
-  } catch (error) {
-    throw new Error(error);
-  }
-};
+    })
+    .then((res) => res.data)
+    .catch(console.error);
