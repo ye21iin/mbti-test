3ea7b51b25869e14c70e09190c657383bf87c5e5
ye21iin <kathykr120@gmail.com>
2025-02-21 19:48:52 +0900

    fix: add Exception handling on api logic

--- src/api/auth.js
+++ src/api/auth.js
@@ -3,34 +3,50 @@ import axios from "axios";
 const API_URL = import.meta.env.VITE_AUTH_BASE_URL;
 
 export const register = async (userData) => {
-  const response = await axios.post(`${API_URL}/register`, userData);
-  return response.data;
+  try {
+    const response = await axios.post(`${API_URL}/register`, userData);
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 const EXP_TM = "5m";
 export const login = async (userData) => {
-  const response = await axios.post(
-    `${API_URL}/login?expiresIn=${EXP_TM}`,
-    userData
-  );
-  return response.data;
+  try {
+    const response = await axios.post(
+      `${API_URL}/login?expiresIn=${EXP_TM}`,
+      userData
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 export const getUserProfile = async (token, id) => {
-  const response = await axios.get(`${API_URL}/user?user_id=${id}`, {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return response.data;
+  try {
+    const response = await axios.get(`${API_URL}/user?user_id=${id}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
 
 export const updateProfile = async (token, formData) => {
-  const response = await axios.patch(`${API_URL}/profile`, formData, {
-    headers: {
-      "Content-Type": "multipart/form-data",
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return response.data;
+  try {
+    const response = await axios.patch(`${API_URL}/profile`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    throw new Error(error);
+  }
 };
