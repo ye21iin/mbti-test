7f568d95353e0100f898071cc770210cb1bddee5
ye21iin <kathykr120@gmail.com>
2025-02-24 16:16:53 +0900

    feat: listing up on results page

--- src/api/testResults.js
+++ src/api/testResults.js
@@ -4,28 +4,24 @@ const JSON_TEST_RESULT_URL = `${
   import.meta.env.VITE_JSON_BASE_URL
 }/testResults`;
 
-export const getTestResults = () => {
+export const getTestResults = () =>
   axios
     .get(JSON_TEST_RESULT_URL)
-    .then((response) => response.data)
+    .then((res) => res.data)
     .catch((error) => console.error(error));
-};
 
-export const createTestResult = (resultData) => {
+export const createTestResult = (resultData) =>
   axios
     .post(JSON_TEST_RESULT_URL, resultData)
-    .then((response) => response.data)
+    .then((res) => res.data)
     .catch((error) => console.error(error));
-};
 
-export const deleteTestResult = (id) => {
+export const deleteTestResult = (id) =>
   axios
     .delete(`JSON_TEST_RESULT_URL/${id}`)
     .catch((error) => console.error(error));
-};
 
-export const updateTestResultVisibility = (id, visibility) => {
+export const updateTestResultVisibility = (id, visibility) =>
   axios
     .patch(`JSON_TEST_RESULT_URL/${id}`, { visibility })
     .catch((error) => console.error(error));
-};
--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { getTestResults } from "../api/testResults";
 
 const Results = () => {
-  return <div>Results</div>;
+  const {
+    data: testResults,
+    isPending: isResultsPending,
+    isError: isResultsError,
+  } = useQuery({
+    queryKey: ["testResults"],
+    queryFn: getTestResults,
+  });
+
+  if (isResultsPending) return <div>Loading...</div>;
+  if (isResultsError) return <div>Error occur...</div>;
+
+  return (
+    <div className="w-full mt-10 flex flex-col items-center justify-center gap-3">
+      <h1 className="text-3xl font-bold mb-6">테스트 결과</h1>
+      {testResults.map((result) => {
+        return (
+          <div
+            className="bg-blue-50 rounded-lg p-8 max-w-lg w-full h-full overflow-y-auto"
+            key={result.id}
+          >
+            <p>{result.userId}</p>
+            <p>{result.mbtiResult}</p>
+            <p>{result.timestamp}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Results;
