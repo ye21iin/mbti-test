1000c4a03793481b80a84b50a1c2b99c3f009ca7
ye21iin <kathykr120@gmail.com>
2025-02-25 12:15:43 +0900

    fix: edit API_URL

--- src/api/configInstance.js
+++ src/api/configInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_URL = import.meta.env.VITE_AUTH_BASE_URL;
-const JSON_TEST_RESULT_URL = `${import.meta.env.VITE_JSON_BASE_URL}`;
+const JSON_URL = `${import.meta.env.VITE_JSON_BASE_URL}`;
 
 export const authApi = axios.create({
   baseURL: API_URL,
